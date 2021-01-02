18c47471209208e426606db5744c0c30b1f0bb62
Viktor <macedonian.wow@live.com>
2021-01-02 14:50:30 +0100

    Option for logged in user to create a book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_174048) do
+ActiveRecord::Schema.define(version: 2021_01_02_133716) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_174048) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
