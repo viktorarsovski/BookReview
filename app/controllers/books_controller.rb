f51df5c22685318985a6fd3230b4c7a342118900
Viktor <macedonian.wow@live.com>
2021-01-02 16:15:06 +0100

    Option to select category for a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,13 @@ class BooksController < ApplicationController
 
   def new
     @book = current_user.books.build
+    @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def create
     @book = current_user.books.build(book_params)
-
+    @book.category_id = params[:category_id]
+    
     if @book.save
       redirect_to root_path
     else
