65a19b088a3f975b6bfe6a4d520539c2ed7799a6
Viktor <macedonian.wow@live.com>
2021-01-10 15:06:14 +0100

    Add user authentications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("jquery")
-//= require("jquery_ujs")
-require("bootstrap")
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-import "channels"
-import "bootstrap-sprockets"
 
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
